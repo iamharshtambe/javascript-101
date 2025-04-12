4d0b992fd57673b3a425919de9c52cce45853d21
iamharshtambe <harshtambe.dev@gmail.com>
2025-04-12 14:20:58 +0530

    completed variables and data types

---
+++ 02-variables-and-data-types.js
@@ -0,0 +1,14 @@
+/*
+variable - a container that stores a value.
+           behaves as if it were the value it contains.
+           
+data type - a particular kind of data item, as defined by the values it can take.
+ */
+
+let myName = 'Harsh Tambe';
+let myAge = 21;
+let isMarried = false;
+
+console.log(myName, myAge, isMarried);
+
+console.log(typeof myName, typeof myAge, typeof isMarried);
